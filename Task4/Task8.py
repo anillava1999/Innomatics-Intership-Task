c042292d0aaea91c71dfaf64b8f9acf44c0f53d5
Anil L <71332138+anillava1999@users.noreply.github.com>
2021-10-19 11:44:26 +0530

    Add files via upload

---
+++ Task4/Task8.py
@@ -0,0 +1,41 @@
+'''
+heck Tutorial tab to know how to to solve.
+You are given a string  and width . 
+Your task is to wrap the string into a paragraph of width .
+Function Description
+Complete the wrap function in the editor below.
+wrap has the following parameters:
+string string: a long string
+int max_width: the width to wrap to
+Returns
+string: a single string with newline characters ('\n') where the breaks should be
+Input Format
+The first line contains a string, . 
+The second line contains the width, .
+Constraints
+
+
+Sample Input 0
+ABCDEFGHIJKLIMNOQRSTUVWXYZ
+4
+Sample Output 0
+ABCD
+EFGH
+IJKL
+IMNO
+QRST
+UVWX
+YZ
+'''
+
+import textwrap
+def wrap(string, max_width):
+    # Text Wrap in Python - HackerRank Solution START
+    return textwrap.fill(string,max_width)
+    # Text Wrap in Python - HackerRank Solution END
+
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
